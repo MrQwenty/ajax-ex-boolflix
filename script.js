02ce303ec0b76354c404ac48fab32fbff019f937
MrQwenty <pelosi.teo@gmail.com>
2018-12-18 15:32:34 +0100

    milestone 1 completed

---
+++ script.js
@@ -0,0 +1,58 @@
+
+function getRatedStars(apiScore) {
+  var rated = [];
+  var score = Math.round(apiScore/2);
+
+    for (var i = 0; i < score; i++) {
+      rated.push('<i class="fas fa-star"></i>')
+    }
+    for (var i = 0; i < (5 - score); i++) {
+      rated.push('<i class="far fa-star"></i>')
+    }
+  return rated.join('');
+}
+
+// MILESTONE 1
+$(document).ready(function () {
+  $('#srcButton').click(function() {
+
+    var searchValue = $('#search').val();
+
+    $.ajax({
+      url : 'https://api.themoviedb.org/3/search/movie',
+      method : 'GET',
+      data : {
+        api_key : '0c70749bec668f742426465c13e8120f',
+        language : 'it-IT',
+        query : searchValue
+      },
+      success : function(callData) {
+
+        $('.results .entry').remove();
+        for (var i = 0; i < callData.results.length; i++) {
+          var source   = $('#entry-template').html();
+          var template = Handlebars.compile(source);
+
+          var stars = getRatedStars(callData.results[i].vote_average);
+
+          var context = {
+            titleVar : callData.results[i].title,
+            originalTitleVar : callData.results[i].original_title,
+            originalLangVar : callData.results[i].original_language,
+            release_dateVar : callData.results[i].release_date,
+            voteAverageVar : getRatedStars(callData.results[i].vote_average)
+          };
+
+          var html = template(context);
+
+          $(".results").append(html);
+
+        }
+
+      },
+      error : function() {
+        console.log('errore');
+      }
+    });
+  });
+});
